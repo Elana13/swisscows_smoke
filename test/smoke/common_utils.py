1f192a3b86af46f77940faa83394d299a4399871
Elana13 <elanagutta@gmail.com>
2021-05-17 20:09:39 +0300

    Chrome driver added

--- test/smoke/common_utils.py
+++ test/smoke/common_utils.py
@@ -1,3 +1,3 @@
 def get_driver_path() -> str:
-    # return 'd:\\ChromeDriver\\chromedriver.exe'
-    return "/mnt/w/github/swisscows_smoke/ubuntu_cr_90/chromedriver"
+    return 'd:\\QA\\Swisscows\\swisscows_smoke\\windows_chrome\\chromedriver.exe'
+    #return "/mnt/w/github/swisscows_smoke/ubuntu_cr_90/chromedriver"
